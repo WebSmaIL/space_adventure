7359db72cf654f26c9017d404abc9738184e2982
Ilya Smakovskiy <96537307+WebSmaIL@users.noreply.github.com>
2023-02-13 23:21:19 +0600

    feat: add solar system

---
+++ client/src/components/solarSystem/Wrapper/Wrapper.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+import Background from '../../../assets/uikit/Background';
+import {background_solar_system} from '../../../assets/img/backgrounds'
+
+interface IProps {
+    children: JSX.Element;
+}
+
+const Wrapper = ({ children }: IProps) => {
+    return (
+        <>
+            <Container>{children}</Container>
+            <Background alt="" src={background_solar_system} />
+        </>
+    );
+};
+
+export default Wrapper;
+
+export const Container = styled.div`
+    position: relative;
+    min-height: 100vh;
+    overflow: hidden;
+`;
